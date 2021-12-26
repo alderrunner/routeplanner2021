fcb7d509a7a9d781cc66016da8b31d5052bd630d
Robin Hagmayer <robin.hagmayer@gmail.com>
2021-12-26 16:56:31 +0100

    Added first project structure & updated .gitignore

---
+++ java_code/RoutePlanner2021Java/src/App.java
@@ -0,0 +1,7 @@
+package java_code.RoutePlanner2021Java.src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
