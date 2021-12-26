c9d42b5568940cb8a359d7ee86ffcccfe54ef6bc
Robin Hagmayer <robin.hagmayer@gmail.com>
2021-12-26 18:54:12 +0100

    Updated Graph with latitude and longitude array

--- java_code/RoutePlanner2021Java/src/graph/Graph.java
+++ java_code/RoutePlanner2021Java/src/graph/Graph.java
@@ -3,10 +3,12 @@ package java_code.RoutePlanner2021Java.src.graph;
 public class Graph {
     private final int[] vertexOffset;
     private final int[][] adjacencyArray;
+    private final double[][] latLong;
 
     public Graph(int numOfNodes, int numOfEdges) {
         this.vertexOffset = new int[numOfNodes];
         this.adjacencyArray = new int[numOfEdges][3];
+        this.latLong = new double[numOfNodes][2];
     }
 
     public int[] getVertexOffset() {
@@ -15,5 +17,9 @@ public class Graph {
 
     public int[][] getAdjacencyArray() {
         return this.adjacencyArray;
+    }
+
+    public double[][] getLatLong() {
+        return this.latLong;
     }
 }
